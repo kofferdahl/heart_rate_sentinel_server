d1bdf0e88f422221c368da8e051fb09e6a98da6b
Katelyn <katelyn.offerdahl@duke.edu>
2018-11-12 16:05:47 -0500

    Adds methods for updating the heart rate, and associated methods for determining if a patient is tachycardiac based on heart rate + ways to get that status

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from pymodm import connect
 from pymodm import MongoModel, fields
+import datetime
 
 connect("mongodb://User:GODUKE10@ds151463.mlab.com:51463/heart-rate-sentinel")
 
@@ -18,3 +19,72 @@ class Patient(MongoModel):
     is_tachycardic = fields.ListField(field=fields.BooleanField())
     heart_rate = fields.ListField(field=fields.IntegerField())
     heart_rate_time = fields.ListField(field=fields.DateTimeField())
+
+
+def update_heart_rate(patient_id, heart_rate):
+    p = Patient.objects.raw({"_id": patient_id}).first()
+
+    p.heart_rate.append(heart_rate)
+    p.heart_rate_time.append(datetime.datetime.now())
+
+    age = p.user_age
+    tachycardic = is_tachycardic(age, heart_rate)
+
+    p.is_tachycardic.append(tachycardic)
+    p.save()
+
+    print(p.is_tachycardic)
+
+
+def is_tachycardic(age, heart_rate):
+
+    if age < 1:
+        if heart_rate > 159:
+            return True
+        else:
+            return False
+    elif age <= 2:
+        if heart_rate > 151:
+            return True
+        else:
+            return False
+    elif age <= 4:
+        if heart_rate > 137:
+            return True
+        else:
+            return False
+    elif age <= 7:
+        if heart_rate > 133:
+            return True
+        else:
+            return False
+    elif age <= 11:
+        if heart_rate > 130:
+            return True
+        else:
+            return False
+    elif age <= 15:
+        if heart_rate > 119:
+            return True
+        else:
+            return False
+    elif age > 15:
+        if heart_rate > 100:
+            return True
+        else:
+            return False
+
+
+def get_status(patient_id):
+    p = Patient.objects.raw({"_id": patient_id}).first()
+
+    output_dict = {}
+    output_dict["is_tachycardic"] = p.is_tachycardic[-1]
+    output_dict["timestamp"] = p.heart_rate_time[-1]
+
+    return output_dict
+
+
+def get_patient(patient_id):
+    p = Patient.objects.raw({"_id": patient_id}).first()
+    return p
--- test_server.py
+++ test_server.py
@@ -1,4 +1,7 @@
-from server import create_patient
+from server import create_patient, is_tachycardic, update_heart_rate, \
+    get_status, get_patient
+import pytest
+import datetime as dt
 
 
 def test_create_patient():
@@ -6,3 +9,53 @@ def test_create_patient():
     assert P.user_age == 10
     assert P.attending_email == "kro18@duke.edu"
     assert P.patient_id == 1
+
+
+@pytest.mark.parametrize("age, heart_rate, expected_tachycardic", [
+    (0, 159, False),
+    (0, 160, True),
+    (1, 151, False),
+    (1, 152, True),
+    (2, 151, False),
+    (2, 152, True),
+    (3, 137, False),
+    (3, 138, True),
+    (4, 137, False),
+    (4, 138, True),
+    (5, 133, False),
+    (5, 134, True),
+    (7, 133, False),
+    (7, 134, True),
+    (8, 130, False),
+    (8, 131, True),
+    (11, 130, False),
+    (11, 131, True),
+    (12, 119, False),
+    (12, 120, True),
+    (15, 119, False),
+    (15, 120, True),
+    (16, 100, False),
+    (16, 101, True),
+    (35, 100, False),
+    (35, 101, True),
+])
+def test_is_tachycardic(age, heart_rate, expected_tachycardic):
+    assert is_tachycardic(age, heart_rate) == expected_tachycardic
+
+
+def test_update_heart_rate(P):
+    update_heart_rate(P.patient_id, 200)
+    p = get_patient(P.patient_id)
+    assert p.heart_rate[-1] == 200
+    assert p.is_tachycardic[-1] is True
+
+
+def test_get_status(P):
+    update_heart_rate(P.patient_id, 200)
+    output_dict = get_status(P.patient_id)
+
+    assert output_dict["is_tachycardic"] is True
+
+    assert output_dict["timestamp"] < dt.datetime.now()
+    assert output_dict["timestamp"] > dt.datetime.now() - dt.timedelta(
+        seconds=10)
